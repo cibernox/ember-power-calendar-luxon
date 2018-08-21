ecf9246cb962b80f3a5f03366cd9528537453714
Miguel Camba <miguel.camba@gmail.com>
2018-08-21 14:24:46 +0200

    Almost working

--- addon/index.js
+++ addon/index.js
@@ -1,13 +1,31 @@
-import { DateTime } from 'luxon';
+import { DateTime, Settings } from "luxon";
+
+function _getWeekdays(dayFormat) {
+  let result = [];
+  let date = new Date(0);
+  let formatter = new Intl.DateTimeFormat(Settings.defaultLocale, { weekday: dayFormat });
+  for (let day = 4; day <= 10; day++) {
+    date.setDate(day);
+    result.push(formatter.format(date));
+  }
+  return result;
+}
 
 export function add(date, quantity, unit) {
   return DateTime.fromJSDate(date)
-    .add({ [unit]: quantity })
+    .plus({ [unit]: quantity })
     .toJSDate();
 }
 
-export function formatDate(/*date, format, locale = null*/) {
-  throw new Error("Not implemented");
+export function formatDate(date, format, locale = null) {
+  let datetime = DateTime.fromJSDate(date);
+  if (locale) {
+    datetime = datetime.setLocale(locale);
+  }
+  format = format.replace('YYYY', 'yyyy')
+  format = format.replace('dddd', 'EEEE');
+  format = format.replace('ddd', 'EEE');
+  return datetime.toFormat(format);
 }
 
 export function startOf(date, unit) {
@@ -23,31 +41,34 @@ export function weekday(date) {
 }
 
 export function isoWeekday(date) {
-  return DateTime.fromJSDate(date).isoWeekday;
+  let dayStr = DateTime.fromJSDate(date).toISOWeekDate().split('-')[2];
+  return parseInt(dayStr, 10);
 }
 
-export function getWeekdaysShort(/**/) {
-  throw new Error("Not implemented");
+export function getWeekdaysShort() {
+  return _getWeekdays('short');
 }
 
-export function getWeekdaysMin(/**/) {
-  throw new Error("Not implemented");
+export function getWeekdaysMin() {
+  return _getWeekdays('narrow');
 }
 
-export function getWeekdays(/**/) {
-  throw new Error("Not implemented");
+export function getWeekdays() {
+  return _getWeekdays('long');
 }
 
-export function isAfter(/*date1, date2*/) {
-  throw new Error("Not implemented");
+export function isAfter(date1, date2) {
+  return +date1 > +date2;
 }
 
-export function isBefore(/*date1, date2*/) {
-  throw new Error("Not implemented");
+export function isBefore(date1, date2) {
+  return +date1 < +date2;
 }
 
-export function isSame(/*date1, date2, unit*/) {
-  throw new Error("Not implemented");
+export function isSame(date1, date2, unit) {
+  let dt1 = DateTime.fromJSDate(date1)
+  let dt2 = DateTime.fromJSDate(date2)
+  return dt1.hasSame(dt2, unit);
 }
 
 export function isBetween(/*date, start, end, unit, inclusivity*/) {
@@ -58,8 +79,16 @@ export function diff(/*date1, date2*/) {
   throw new Error("Not implemented");
 }
 
-export function normalizeDate(/*dateOrMoment*/) {
-  throw new Error("Not implemented");
+export function normalizeDate(dateOrDatetime) {
+  if (
+    dateOrDatetime === undefined ||
+    dateOrDatetime === null ||
+    dateOrDatetime instanceof Date
+  ) {
+    return dateOrDatetime;
+  } else {
+    return dateOrDatetime.toJSDate();
+  }
 }
 
 export function normalizeRangeActionValue(/*val*/) {
@@ -70,12 +99,22 @@ export function normalizeMultipleActionValue(/*val*/) {
   throw new Error("Not implemented");
 }
 
-export function normalizeCalendarDay(/*day*/) {
-  throw new Error("Not implemented");
+export function normalizeCalendarDay(day) {
+  day.luxon = DateTime.fromJSDate(day.date);
+  return day;
 }
 
-export function withLocale(/*locale, fn*/) {
-  throw new Error("Not implemented");
+export function withLocale(locale, fn) {
+  let returnValue;
+  if (locale) {
+    let previousLocale = Settings.defaultLocale;
+    Settings.defaultLocale = locale;
+    returnValue = fn();
+    Settings.defaultLocale = previousLocale;
+  } else {
+    returnValue = fn();
+  }
+  return returnValue;
 }
 
 export function normalizeCalendarValue(/*value*/) {
@@ -86,6 +125,6 @@ export function normalizeDuration(/*value*/) {
   throw new Error("Not implemented");
 }
 
-export function getDefaultLocale(/**/) {
-  throw new Error("Not implemented");
+export function getDefaultLocale() {
+  return Settings.defaultLocale || DateTime.local().resolvedLocaleOpts().locale;
 }
