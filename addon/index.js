e8cbef9341eed57a4906e0e43cab5ec2cd1eb840
Miguel Camba <miguel.camba@gmail.com>
2018-08-24 14:52:47 +0200

    v0.1.2

--- addon/index.js
+++ addon/index.js
@@ -1,5 +1,132 @@
 import { DateTime, Settings, Duration } from 'luxon';
 
+/* Sadly the day in which weeks start is not part of the Intl API, so I need to harcode them */
+const WEEK_STARTS = {
+  af: 1,
+  ar: 6,
+  'ar-dz': 0,
+  'ar-kw': 0,
+  'ar-ly': 6,
+  'ar-ma': 6,
+  'ar-sa': 0,
+  'ar-tn': 1,
+  az: 1,
+  be: 1,
+  bg: 1,
+  bm: 1,
+  bn: 0,
+  bo: 0,
+  br: 1,
+  bs: 1,
+  ca: 1,
+  cs: 1,
+  cv: 1,
+  cy: 1,
+  da: 1,
+  de: 1,
+  'de-at': 1,
+  'de-ch': 1,
+  dv: 7,
+  el: 1,
+  en: 0,
+  'en-au': 1,
+  'en-ca': 0,
+  'en-gb': 1,
+  'en-ie': 1,
+  'en-il': 0,
+  'en-nz': 1,
+  eo: 1,
+  es: 1,
+  'es-do': 1,
+  'es-us': 0,
+  et: 1,
+  eu: 1,
+  fa: 6,
+  fi: 1,
+  fo: 1,
+  fr: 1,
+  'fr-ca': 0,
+  'fr-ch': 1,
+  fy: 1,
+  gd: 1,
+  gl: 1,
+  'gom-latn': 1,
+  gu: 0,
+  he: 0,
+  hi: 0,
+  hr: 1,
+  hu: 1,
+  'hy-am': 1,
+  id: 1,
+  is: 1,
+  it: 1,
+  ja: 0,
+  jv: 1,
+  ka: 1,
+  kk: 1,
+  km: 1,
+  kn: 0,
+  ko: 0,
+  ky: 1,
+  lb: 1,
+  lo: 0,
+  lt: 1,
+  lv: 1,
+  me: 1,
+  mi: 1,
+  mk: 1,
+  ml: 0,
+  mn: 0,
+  mr: 0,
+  ms: 1,
+  'ms-my': 1,
+  mt: 1,
+  my: 1,
+  nb: 1,
+  ne: 0,
+  nl: 1,
+  'nl-be': 1,
+  nn: 1,
+  'pa-in': 0,
+  pl: 1,
+  pt: 1,
+  'pt-br': 0,
+  ro: 1,
+  ru: 1,
+  sd: 1,
+  se: 1,
+  si: 0,
+  sk: 1,
+  sl: 1,
+  sq: 1,
+  sr: 1,
+  'sr-cyrl': 1,
+  ss: 1,
+  sv: 1,
+  sw: 1,
+  ta: 0,
+  te: 0,
+  tet: 1,
+  tg: 1,
+  th: 0,
+  'tl-ph': 1,
+  tlh: 1,
+  tr: 1,
+  tzl: 1,
+  tzm: 6,
+  'tzm-latn': 6,
+  'ug-cn': 1,
+  uk: 1,
+  ur: 1,
+  uz: 1,
+  'uz-latn': 1,
+  vi: 1,
+  yo: 1,
+  'zh-cn': 1,
+  'zh-hk': 0,
+  'zh-tw': 0
+};
+
 function _getWeekdays(dayFormat) {
   let result = [];
   let date = new Date(0);
@@ -30,7 +157,18 @@ export function formatDate(date, format, locale = null) {
 }
 
 export function startOf(date, unit) {
-  return DateTime.fromJSDate(date).startOf(unit).toJSDate();
+  let datetime = DateTime.fromJSDate(date);
+  if (unit === 'week') {
+    let normalizedLocale = datetime.locale.toLowerCase();
+    let weekday = WEEK_STARTS[normalizedLocale];
+    if (weekday === undefined) {
+      weekday = 0;
+    }
+    datetime = datetime.set({ hour: 0, minute: 0, second: 0, millisecond: 0, weekday });
+  } else {
+    datetime = datetime.startOf(unit);
+  }
+  return datetime.toJSDate();
 }
 
 export function endOf(date, unit) {
