d04f8fa15c9fa12bafc2b4695845605693bb6eb1
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-01-03 15:57:25 +0100

    Addon to V2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,1 @@
-## 0.4.0
-- Many updates, including ember-auto-import v2, which may be a breaking change.
-## 0.3.0
-- Update luxon to v3
+ember-power-calendar-luxon/CHANGELOG.md
--- tests/dummy/app/helpers/set-datetime.js
+++
@@ -1,8 +0,0 @@
-import { helper } from '@ember/component/helper';
-import { set } from '@ember/object';
-
-export default helper(function setDatetime([that, field, value]) {
-  if (that && field && value) {
-    set(that, field, value.datetime);
-  }
-});
