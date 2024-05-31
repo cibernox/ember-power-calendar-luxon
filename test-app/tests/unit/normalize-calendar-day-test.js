91fd2c69c5ffb886814e8be5514db9ebf18199b4
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-05-31 07:56:21 +0200

    Add some tests

---
+++ test-app/tests/unit/normalize-calendar-day-test.js
@@ -0,0 +1,33 @@
+import { module, test } from 'qunit';
+import { normalizeCalendarDay } from 'ember-power-calendar/test-support/helpers';
+import { Settings } from 'luxon';
+
+const defaultZone = Settings.defaultZone.name;
+
+module('Unit | Utility | date-utils', function (hooks) {
+  hooks.beforeEach(function () {
+    Settings.defaultZone = 'Pacific/Niue';
+  });
+
+  hooks.afterEach(function () {
+    Settings.defaultZone = defaultZone;
+  });
+
+  test('normalizeCalendarDay', function (assert) {
+    let date = new Date('2020-01-02T00:00:00Z');
+    
+    const calendarDay = normalizeCalendarDay({
+      id: 1,
+      number: 1,
+      date: date,
+      isDisabled: false,
+      isFocused: false,
+      isCurrentMonth: false,
+      isToday: false,
+      isSelected: false,
+    });
+
+    assert.equal(calendarDay.datetime.zoneName, 'Pacific/Niue');
+    assert.equal(calendarDay.datetime.toISO(), '2020-01-01T13:00:00.000-11:00');
+  });
+});
