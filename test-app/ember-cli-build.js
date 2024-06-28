44c8610d2c4b50a1ac9dd7cc6329f1f47a9849fd
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-06-28 09:45:38 +0200

    Try to fix embroider

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
@@ -10,5 +11,15 @@ module.exports = function (defaults) {
   });
 
   const { maybeEmbroider } = require('@embroider/test-setup');
-  return maybeEmbroider(app);
+  return maybeEmbroider(app, {
+    packagerOptions: {
+      webpackConfig: {
+        resolve: {
+          alias: {
+            luxon: path.resolve(__dirname, 'node_modules/luxon/build/node/luxon.js'),
+          },
+        },
+      },
+    },
+  });
 };
